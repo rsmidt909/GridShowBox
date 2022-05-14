40baa19885abb66a8659897fa4af08821b6eb517
Shawn <rsmidt909@hotmail.com>
2022-05-13 23:36:13 -0500

    fixed space issue in test command, added all sections for genre

--- GridShowBox_install.sh
+++ GridShowBox_install.sh
@@ -6,8 +6,10 @@ GRID_HOME_DIR=/home/${USER}/GridShowBox
 
 #Check if FS exists at user home and if not, create it
 mainFSCheck(){
+    echo $USER
+    echo $GRID_HOME_DIR
     echo "The Grid Show Box dir Check"
-[[ ! -d $GRID_HOME_DIR ]] && mkdir $GRID_HOME_DIR 
+[[ ! -d ${GRID_HOME_DIR} ]] && mkdir $GRID_HOME_DIR
 }
 
 mainFSCheck
@@ -15,7 +17,7 @@ mainFSCheck
 #check if TheGrid directory exists within GridShowBox
 theGridDirCheck(){
     echo "The Grid Dir Check"
-    [[ ! -d ${GRID_HOME_DIR}/TheGrid]] && mkdir ${GRID_HOME_DIR}/TheGrid
+[[ ! -d ${GRID_HOME_DIR}/TheGrid ]] && mkdir ${GRID_HOME_DIR}/TheGrid
 }
 
 theGridDirCheck
@@ -23,15 +25,19 @@ theGridDirCheck
 #check if TheGrid category directories exist
 theGridCatCheck(){
     echo 'The Grid Cats Check'
-    [[ ! -d ${GRID_HOME_DIR}/TheGrid/TV]] && mkdir ${GRID_HOME_DIR}/TheGrid/TV ${GRID_HOME_DIR}/TheGrid/TV/Genre ${GRID_HOME_DIR}/TheGrid/TV/Genre/Action ${GRID_HOME_DIR}/TheGrid/TV/Genre/Comedy ${GRID_HOME_DIR}/TheGrid/TV/Genre/Drama ${GRID_HOME_DIR}/TheGrid/TV/Genre/Fantasy ${GRID_HOME_DIR}/TheGrid/TV/Genre/Horror ${GRID_HOME_DIR}/TheGrid/TV/Genre/Romance
-#    [[ ! -d ${GRID_HOME_DIR}/TheGrid/Movies]] && mkdir ${GRID_HOME_DIR}/TheGrid/Movies ${GRID_HOME_DIR}/TheGrid/Movies/Genre ${GRID_HOME_DIR}/TheGrid/Movies/Genre/Action ${GRID_HOME_DIR}/TheGrid/Movies/Genre/Comedy ${GRID_HOME_DIR}/TheGrid/Movies/Genre/Drama ${GRID_HOME_DIR}/TheGrid/Movies/Genre/Fantasy ${GRID_HOME_DIR}/TheGrid/Movies/Genre/Horror ${GRID_HOME_DIR}/TheGrid/Movies/Genre/Romance
-#    [[ ! -d ${GRID_HOME_DIR}/TheGrid/Commercials]] && mkdir ${GRID_HOME_DIR}/TheGrid/Commercials ${GRID_HOME_DIR}/TheGrid/Commercials/Genre ${GRID_HOME_DIR}/TheGrid/Commercials/Genre/Action ${GRID_HOME_DIR}/TheGrid/Commercials/Genre/Comedy ${GRID_HOME_DIR}/TheGrid/Commercials/Genre/Drama ${GRID_HOME_DIR}/TheGrid/Commercials/Genre/Fantasy ${GRID_HOME_DIR}/TheGrid/Commercials/Genre/Horror ${GRID_HOME_DIR}/TheGrid/Commercials/Genre/Romance
+    [[ ! -d ${GRID_HOME_DIR}/TheGrid/TV ]] && mkdir ${GRID_HOME_DIR}/TheGrid/TV ${GRID_HOME_DIR}/TheGrid/TV/Genre ${GRID_HOME_DIR}/TheGrid/TV/Genre/Action ${GRID_HOME_DIR}/TheGrid/TV/Genre/Comedy ${GRID_HOME_DIR}/TheGrid/TV/Genre/Drama ${GRID_HOME_DIR}/TheGrid/TV/Genre/Fantasy ${GRID_HOME_DIR}/TheGrid/TV/Genre/Horror ${GRID_HOME_DIR}/TheGrid/TV/Genre/Romance
+    [[ ! -d ${GRID_HOME_DIR}/TheGrid/Movies ]] && mkdir ${GRID_HOME_DIR}/TheGrid/Movies ${GRID_HOME_DIR}/TheGrid/Movies/Genre ${GRID_HOME_DIR}/TheGrid/Movies/Genre/Action ${GRID_HOME_DIR}/TheGrid/Movies/Genre/Comedy ${GRID_HOME_DIR}/TheGrid/Movies/Genre/Drama ${GRID_HOME_DIR}/TheGrid/Movies/Genre/Fantasy ${GRID_HOME_DIR}/TheGrid/Movies/Genre/Horror ${GRID_HOME_DIR}/TheGrid/Movies/Genre/Romance
+    [[ ! -d ${GRID_HOME_DIR}/TheGrid/Commercials ]] && mkdir ${GRID_HOME_DIR}/TheGrid/Commercials ${GRID_HOME_DIR}/TheGrid/Commercials/Genre ${GRID_HOME_DIR}/TheGrid/Commercials/Genre/Action ${GRID_HOME_DIR}/TheGrid/Commercials/Genre/Comedy ${GRID_HOME_DIR}/TheGrid/Commercials/Genre/Drama ${GRID_HOME_DIR}/TheGrid/Commercials/Genre/Fantasy ${GRID_HOME_DIR}/TheGrid/Commercials/Genre/Horror ${GRID_HOME_DIR}/TheGrid/Commercials/Genre/Romance
 }
 
 theGridCatCheck
 
 
 workspaceCheck(){
-    [[ ! -d ${GRID_HOME_DIR}/workspace ]] && mkdir ${GRID_HOME_DIR}/workspace
+    [[ ! -d ${GRID_HOME_DIR}/Workspace ]] && mkdir ${GRID_HOME_DIR}/Workspace
 }
+
+
+workspaceCheck
+
 
