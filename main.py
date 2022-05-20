72a9995d0ff721d1a53794f9d2c1a4f2e7c8a1f9
Shawn <rsmidt909@hotmail.com>
2022-05-20 18:25:02 -0500

    cleaned up unused methods. Cleaned up unused variables. Added comments to everything present.

--- main.py
+++ main.py
@@ -3,20 +3,30 @@ import json
 import time
 import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-encoding="locale"
+
+#name of user
 user = "rinzler"
+
+#local variable of schedule home
 schedule_home = "/home/" + user +"/GridShowBox/Schedule"
-schedule = {}
-list_of_videos = {}
+
+
+#cached array of videos to play
+list_of_videos = []
+
+#cached version of localtime in object form on machine
 time_object = time.localtime(time.time())
+#py object of time with hour, min, and current time. Note: Leading Zero's do not exist. EX. 2:14, 9:3, 16:50,
 grid_clock = {
     "hour": time_object.tm_hour,
     "min": time_object.tm_min,
     "current_time": str(time_object.tm_hour) + ":" + str(time_object.tm_min)
 }
+
+#length of time currently used by GridStream
 grid_length = 0
+
+#Video class params that all videos will be subject to having.
 class Video:
     Running = False,
     Length = "",
@@ -25,7 +35,7 @@ class Video:
     PostTag = "",
     Tag = "",
     Holiday = "",
-    PrimID = 0,
+    PrimeID = 0,
     Path = "",
     #Below are required
     Title = "",
@@ -33,42 +43,54 @@ class Video:
     Season = "",
     Genre = ""
 
+#cached variable of if today is a holiday from list_of_holidays
 holiday = False
-list_of_recentVideoIDs = []
-count = 0
+
+#Today's date in month/day format. Note: there are no zeros. EX. January = 1, August = 8
 today = str(time_object.tm_mon) + "/" + str(time_object.tm_mday)
-listOfHolidays = [
+
+
+#cached list of local holidays from /home/rinzler/GridShowBox/TheGrid/Count.txt
+list_of_holidays = [
     "3/4",
     "4/5",
     "6/15"
 ]
 
-videoID = 0
 
+#local cache of last video_id used and count
+video_id = 0
+count = 0
+
+#cached version of the MasterFile in py object form
 cached_video_list_py = []
 
-def UpdateMasterFile(incomingData):
+#array of random numbers to choose for non-commercial stream
+random_set_list = []
+
+
+
+#**********************************************************************************
+
+def update_masterfile(incomingData):
     with open('/home/rinzler/GridShowBox/TheGrid/MasterFile.json' 'r') as file:
 
 
 
-#creates a file names Count.txt and updates the number
-def UpdateVideoCount():
+#creates a file names Count.txt and updates the number NEEDS REWORKED
+def update_video_count():
     with open('/home/rinzler/GridShowBox/TheGrid/Count.txt' 'w' ) as file:
-        numberStr = file.read()
-        numberInt = int(numberStr)
-        newNumberInt = numberInt + 1
-        count = newNumberInt
-        newNumberStr = str(newNumberInt)
-        file.write(newNumberStr)
-
-#plays the loaded video in the stream
-#def playVideoStream():
+        number_str = file.read()
+        number_int = int(number_str)
+        new_number_int = number_int + 1
+        count = new_number_int
+        new_number_str = str(new_number_int)
+        file.write(new_number_str)
 
 
-
-def isTodayAHoliday():
-    for x in listOfHolidays:
+#makes global variable holiday true or false from list_of_holidays
+def is_today_a_holiday():
+    for x in list_of_holidays:
         if x == today:
             holiday = True
         else:
@@ -76,7 +98,7 @@ def isTodayAHoliday():
 
 
 
-#convert /home/rinzler/GridShowBox/MasterFile.json to an interable python pbject
+#convert /home/rinzler/GridShowBox/MasterFile.json to an iterable python object and cache
 def make_masterfile_py():
     with open('/home/rinzler/GridShowBox/MasterFile.json' 'r' ) as file:
         video_list_raw = json.dumps(file)
@@ -89,36 +111,18 @@ def check_cached_video_py_contents():
 
 
 #return video object from ID
-def grabVideo(primid):
+def grab_video(primid):
     check_cached_video_py_contents()
     video_object = Video()
         for x in cached_video_list_py:
             video_object = x
             if x.PrimeID == primid:
-                
 
-# create the schedule
-def createSchedule():
-    for x in
-    listOfVideos.apend()
-    schedule = {
-        "Monday": listOfEpisodes
-    }
-
-#check if the schedule exists
-#def checkSchedule():
-
-
-#parses schedule and returns the current video and next video data
-def parseSchedule():
-    currentVideo
-    nextVideo
+#call random number 50 times for random_set_list
 
 
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(gridClock)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(grid_clock)
