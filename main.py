53283e5b310366fd04bdb1bda1282ab5884c9aac
Shawn <rsmidt909@hotmail.com>
2022-05-20 19:07:59 -0500

    cleaned up code and adheated to python standarsds. Created roll of 50 random numbers, gave all open method linux utf-8 encoding, cleaned up is today a holiday method

--- main.py
+++ main.py
@@ -4,29 +4,29 @@ import time
 import random
 
 
-#name of user
+# name of user
 user = "rinzler"
 
-#local variable of schedule home
-schedule_home = "/home/" + user +"/GridShowBox/Schedule"
+# local variable of schedule home
+schedule_home = "/home/" + user + "/GridShowBox/Schedule"
 
-
-#cached array of videos to play
+# cached array of videos to play
 list_of_videos = []
 
-#cached version of localtime in object form on machine
+# cached version of localtime in object form on machine
 time_object = time.localtime(time.time())
-#py object of time with hour, min, and current time. Note: Leading Zero's do not exist. EX. 2:14, 9:3, 16:50,
+# py object of time with hour, min, and current time. Note: Leading Zero's do not exist. EX. 2:14, 9:3, 16:50,
 grid_clock = {
     "hour": time_object.tm_hour,
     "min": time_object.tm_min,
     "current_time": str(time_object.tm_hour) + ":" + str(time_object.tm_min)
 }
 
-#length of time currently used by GridStream
+# length of time currently used by GridStream
 grid_length = 0
 
-#Video class params that all videos will be subject to having.
+
+# Video class params that all videos will be subject to having.
 class Video:
     Running = False,
     Length = "",
@@ -37,20 +37,20 @@ class Video:
     Holiday = "",
     PrimeID = 0,
     Path = "",
-    #Below are required
+    # Below are required
     Title = "",
     Episode = "",
     Season = "",
     Genre = ""
 
-#cached variable of if today is a holiday from list_of_holidays
-holiday = False
 
-#Today's date in month/day format. Note: there are no zeros. EX. January = 1, August = 8
+# cached variable of if today is a holiday from list_of_holidays
+holiday = False
+# Today's date in month/day format. Note: there are no zeros. EX. January = 1, August = 8
 today = str(time_object.tm_mon) + "/" + str(time_object.tm_mday)
 
 
-#cached list of local holidays from /home/rinzler/GridShowBox/TheGrid/Count.txt
+# cached list of local holidays from /home/rinzler/GridShowBox/TheGrid/Count.txt
 list_of_holidays = [
     "3/4",
     "4/5",
@@ -58,28 +58,22 @@ list_of_holidays = [
 ]
 
 
-#local cache of last video_id used and count
+# local cache of last video_id used and count
 video_id = 0
 count = 0
 
-#cached version of the MasterFile in py object form
-cached_video_list_py = []
+# cached version of the MasterFile in py object form
+cached_video_list_py = ()
 
-#array of random numbers to choose for non-commercial stream
+# array of random numbers to choose for non-commercial stream
 random_set_list = []
 
+# __________________________________________________________________________
 
 
-#**********************************************************************************
-
-def update_masterfile(incomingData):
-    with open('/home/rinzler/GridShowBox/TheGrid/MasterFile.json' 'r') as file:
-
-
-
-#creates a file names Count.txt and updates the number NEEDS REWORKED
+# creates a file names Count.txt and updates the number NEEDS REWORKED
 def update_video_count():
-    with open('/home/rinzler/GridShowBox/TheGrid/Count.txt' 'w' ) as file:
+    with open('/home/rinzler/GridShowBox/TheGrid/Count.txt' 'w' 'utf-8' ) as file:
         number_str = file.read()
         number_int = int(number_str)
         new_number_int = number_int + 1
@@ -88,39 +82,41 @@ def update_video_count():
         file.write(new_number_str)
 
 
-#makes global variable holiday true or false from list_of_holidays
+# makes global variable holiday true or false from list_of_holidays
 def is_today_a_holiday():
     for x in list_of_holidays:
         if x == today:
+            global holiday
             holiday = True
-        else:
-            holiday = False
+            break
 
 
+# check Masterfile exist, if not then make
+def check_masterfile():
+    with open('/home/rinzler/TheGridSBox/MasterFile.json' 'w' 'utf-8') as file:
+        print("MasterFile Check")
 
-#convert /home/rinzler/GridShowBox/MasterFile.json to an iterable python object and cache
+
+# convert /home/rinzler/GridShowBox/MasterFile.json to an iterable python object and cache
 def make_masterfile_py():
-    with open('/home/rinzler/GridShowBox/MasterFile.json' 'r' ) as file:
-        video_list_raw = json.dumps(file)
+    with open('/home/rinzler/GridShowBox/MasterFile.json' 'r' 'utf-8') as file:
+        global cached_video_list_py
+        video_list_raw = file
         cached_video_list_py = json.load(video_list_raw)
 
-#check if the cached_video_list_py is empty. If so, run make_masterfile
+
+# check if the cached_video_list_py is empty. If so, run make_masterfile
 def check_cached_video_py_contents():
     if cached_video_list_py.count == 0:
         make_masterfile_py()
 
 
-#return video object from ID
-def grab_video(primid):
-    check_cached_video_py_contents()
-    video_object = Video()
-        for x in cached_video_list_py:
-            video_object = x
-            if x.PrimeID == primid:
-
-#call random number 50 times for random_set_list
-
-
+# call random number 50 times for random_set_list
+def roll_50_random_numbers():
+    for x in range(50):
+        random_number = random.randint(1, count)
+        random_set_list.append(random_number)
+        print(random_number)
 
 
 # Press the green button in the gutter to run the script.
